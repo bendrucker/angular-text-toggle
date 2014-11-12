8b447f9f5850f5e3f4444b9b932850aba73b8b36
Ben Drucker <bvdrucker@gmail.com>
2014-11-12 11:00:12 -0500

    Add test against action triggering toggle

--- test/index.js
+++ test/index.js
@@ -38,7 +38,15 @@ describe('text-toggle', function () {
     expect(element.find('toggle-confirmation').css('display')).to.be.empty;
   });
 
-  it('can be toggled via an undo', function () {
+  it('can be turned on via the action', function () {
+    scope.condition = false;
+    var element = $compile(normal)(scope);
+    element.find('toggle-action').triggerHandler('click');
+    scope.$digest();
+    expect(scope.condition).to.be.true;  
+  });
+
+  it('can be turned off via an undo', function () {
     scope.condition = true;
     var element = $compile(normal)(scope);
     scope.$digest();
