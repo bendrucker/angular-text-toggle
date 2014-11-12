891f58588c6d1bc6e58e1b33239970a865e9750d
Ben Drucker <bvdrucker@gmail.com>
2014-11-12 08:29:15 -0500

    init

---
+++ karma.conf.js
@@ -0,0 +1,21 @@
+'use strict';
+
+module.exports = function (config) {
+  config.set({
+    frameworks: ['browserify', 'mocha', 'chai'],
+    files: [
+      './node_modules/angular/angular.js',
+      './node_modules/angular-mocks/angular-mocks.js',
+      './test/*.js'
+    ],
+    preprocessors: {
+      './test/*.js': ['browserify']
+    },
+    browserify: {
+      debug: true,
+      transform: ['browserify-istanbul', 'browserify-shim']
+    },
+    reporters: ['progress', 'coverage'],
+    browsers: ['PhantomJS'],
+  });
+};
